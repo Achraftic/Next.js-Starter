a7b58dc088df0214d97691999c832c17cafa7046
Achraftic <ticachraf@gmail.com>
2025-03-16 14:48:09 +0000

    fix hydration prblm

--- src/components/ToggleTheme.tsx
+++ src/components/ToggleTheme.tsx
@@ -1,30 +1,47 @@
-'use client'
-import React from 'react';
+"use client";
+
+import React, { useEffect, useState } from "react";
 import { Button } from "@/components/ui/button";
-import { Moon, Sun } from "lucide-react"
-import { useTheme } from 'next-themes';
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+
 const ToggleTheme = () => {
     const { theme, setTheme } = useTheme();
+    const [mounted, setMounted] = useState(false);
+
+    // Prevent hydration mismatch by setting the state only on mount
+    useEffect(() => {
+        setMounted(true);
+    }, []);
+
     const toggleTheme = () => {
-        if (theme === "dark") {
-            setTheme("light");
-        }
-        else {
-            setTheme("dark");
-        }
-    }
+        setTheme(theme === "dark" ? "light" : "dark");
+    };
+
+    // Ensure it only renders icons after mounting to avoid hydration mismatch
+    if (!mounted) return null;
+
     return (
-       
-            <Button asChild onClick={toggleTheme} variant="ghost" size="icon" className='absolute top-6 right-6 group'>
-
-                {theme === "dark" ? 
-                <Sun id="sun"  className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all group-focus:rotate-12 " />: 
-                <Moon id="moon" className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all group-focus:rotate-12 " />
-                }
-
-            </Button>
-   
+        <Button
+            asChild
+            onClick={toggleTheme}
+            variant="ghost"
+            size="icon"
+            className="absolute top-6 right-6 group"
+        >
+            {theme === "dark" ? (
+                <Sun
+                    id="sun"
+                    className="h-[1rem] w-[1rem] rotate-0 scale-100 transition-all group-focus:rotate-12"
+                />
+            ) : (
+                <Moon
+                    id="moon"
+                    className="h-[1rem] w-[1rem] rotate-0 scale-100 transition-all group-focus:rotate-12"
+                />
+            )}
+        </Button>
     );
-}
+};
 
 export default ToggleTheme;
